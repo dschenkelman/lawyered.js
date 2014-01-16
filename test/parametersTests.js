296251302392553d6dc659244a7eb5a83bf98987
dschenkelman <damian.schenkelman@gmail.com>
2014-01-15 20:34:15 -0800

    Renamed to lawyered.js. Added lawyered.sublime-project. Implemented ability to receive parameters and this in preconditions, postconditions and original method. Implemented ability to receive return value in post conditions.

---
+++ test/parametersTests.js
@@ -0,0 +1,149 @@
+var should = require('should'),
+contracts = require('../lib/index.js');
+
+describe('contracts', function(){
+	describe('prencondition parameters', function(){
+		it('should pass method parameters to precondition', function(){
+			var params = [];
+
+			var obj = {
+					m: function(p1, p2, p3){
+					},
+					m_pre: function(p1, p2, p3){
+						params.push(p1);
+						params.push(p2);
+						params.push(p3);
+					}
+				};
+
+			contracts.instrument(obj);
+
+			obj.m(1, 2, 3);
+
+			params.length.should.equal(3);
+			params[0].should.equal(1);
+			params[1].should.equal(2);
+			params[2].should.equal(3);
+		});
+		it('should pass correct instance to precondition', function(){
+			var instance = null;
+
+			var obj = {
+					m: function(){
+					},
+					m_pre: function(){
+						instance = this;
+					}
+				};
+
+			contracts.instrument(obj);
+
+			obj.m();
+
+			obj.should.equal(instance);
+		});
+	});
+
+	describe('method parameters', function(){
+		it('should pass method parameters to method', function(){
+			var params = [];
+
+			var obj = {
+					m: function(p1, p2, p3){
+						params.push(p1);
+						params.push(p2);
+						params.push(p3);
+					},
+					m_pre: function(p1, p2, p3){
+					}
+				};
+
+			contracts.instrument(obj);
+
+			obj.m(1, 2, 3);
+
+			params.length.should.equal(3);
+			params[0].should.equal(1);
+			params[1].should.equal(2);
+			params[2].should.equal(3);
+		});
+
+		it('should pass correct instance to original method', function(){
+			var instance = null;
+
+			var obj = {
+					m: function(){
+						instance = this;
+					},
+					m_pre: function(){
+					}
+				};
+
+			contracts.instrument(obj);
+
+			obj.m();
+
+			obj.should.equal(instance);
+		});
+	});
+
+	describe('postcondition parameters', function(){
+		it('should pass method parameters to postcondition', function(){
+			var params = [];
+
+			var obj = {
+					m: function(p1, p2, p3){
+					},
+					m_post: function(p1, p2, p3){
+						params.push(p1);
+						params.push(p2);
+						params.push(p3);
+					}
+				};
+
+			contracts.instrument(obj);
+
+			obj.m(1, 2, 3);
+
+			params.length.should.equal(3);
+			params[0].should.equal(1);
+			params[1].should.equal(2);
+			params[2].should.equal(3);
+		});
+		it('should pass correct instance to postcondition', function(){
+			var instance = null;
+
+			var obj = {
+					m: function(){
+					},
+					m_post: function(){
+						instance = this;
+					}
+				};
+
+			contracts.instrument(obj);
+
+			obj.m();
+
+			obj.should.equal(instance);
+		});
+		it('should pass return value as last postcondition parameter', function(){
+			var toReturn = "returnValue";
+
+			var obj = {
+					m: function(p1, p2, p3){
+						return toReturn;
+					},
+					m_post: function(p1, p2, p3, returnValue){
+						returnedValue = returnValue;
+					}
+				};
+
+			contracts.instrument(obj);
+
+			obj.m(1, 2, 3);
+
+			toReturn.should.equal(returnedValue);
+		});
+	});
+});
